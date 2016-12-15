fa8eb398a6b69c7177ce4fe3568764b2f9afb4d0
Lilichuan <luffy74123@gmail.com>
2016-12-15 10:02:30 +0800

    Back up

---
+++ app/src/main/java/com/taiwanandroidapp/tim/bopomofotoenglish/TransImService.java
@@ -0,0 +1,23 @@
+package com.taiwanandroidapp.tim.bopomofotoenglish;
+
+import android.inputmethodservice.InputMethodService;
+import android.view.View;
+
+/**
+ * Created by tim on 2016/12/15.
+ */
+
+public class TransImService extends InputMethodService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public View onCreateInputView() {
+
+
+
+        return super.onCreateInputView();
+    }
+}
---
+++ app/src/main/java/com/taiwanandroidapp/tim/bopomofotoenglish/UI/MyKeyboardView.java
@@ -0,0 +1,20 @@
+package com.taiwanandroidapp.tim.bopomofotoenglish.UI;
+
+import android.content.Context;
+import android.inputmethodservice.KeyboardView;
+import android.util.AttributeSet;
+
+
+public class MyKeyboardView extends KeyboardView{
+
+    public MyKeyboardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyKeyboardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+}
