3d21b99537e462426fc7ce913d9c4c07193613a0
Lilichuan <luffy74123@gmail.com>
2016-12-16 10:13:53 +0800

    Back up

--- app/src/main/java/com/taiwanandroidapp/tim/bopomofotoenglish/TransImService.java
+++ app/src/main/java/com/taiwanandroidapp/tim/bopomofotoenglish/TransImService.java
@@ -1,23 +1,34 @@
 package com.taiwanandroidapp.tim.bopomofotoenglish;
 
 import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
 import android.view.View;
+
+import com.taiwanandroidapp.tim.bopomofotoenglish.UI.MyKeyboardView;
 
 /**
  * Created by tim on 2016/12/15.
  */
 
 public class TransImService extends InputMethodService {
+
+    private Keyboard bopomofo_keyboard;
+
     @Override
     public void onCreate() {
         super.onCreate();
     }
 
     @Override
+    public void onInitializeInterface() {
+        super.onInitializeInterface();
+
+        bopomofo_keyboard = new Keyboard(this,R.xml.bopomofo_keys);
+
+    }
+
+    @Override
     public View onCreateInputView() {
-
-
-
-        return super.onCreateInputView();
+        return getLayoutInflater().inflate(R.layout.my_keyboard_view, null, false);
     }
 }
--- app/src/main/java/com/taiwanandroidapp/tim/bopomofotoenglish/UI/MyKeyboardView.java
+++ app/src/main/java/com/taiwanandroidapp/tim/bopomofotoenglish/UI/MyKeyboardView.java
@@ -1,11 +1,16 @@
 package com.taiwanandroidapp.tim.bopomofotoenglish.UI;
 
 import android.content.Context;
+import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.util.AttributeSet;
 
 
 public class MyKeyboardView extends KeyboardView{
+
+    static final int KEYCODE_OPTIONS = -100;
+    // TODO: Move this into android.inputmethodservice.Keyboard
+    static final int KEYCODE_LANGUAGE_SWITCH = -101;
 
     public MyKeyboardView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -13,6 +18,16 @@ public class MyKeyboardView extends KeyboardView{
 
     public MyKeyboardView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected boolean onLongPress(Keyboard.Key key) {
+        if (key.codes[0] == Keyboard.KEYCODE_CANCEL) {
+            getOnKeyboardActionListener().onKey(KEYCODE_OPTIONS, null);
+            return true;
+        } else {
+            return super.onLongPress(key);
+        }
     }
 
 
