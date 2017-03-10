7a792b5604a63c361859a90b673b778783e268aa
Lilichuan <luffy74123@gmail.com>
2017-03-10 21:06:42 +0800

    Back up

--- app/src/main/java/com/taiwanandroidapp/tim/bopomofotoenglish/TransImService.java
+++ app/src/main/java/com/taiwanandroidapp/tim/bopomofotoenglish/TransImService.java
@@ -1,5 +1,6 @@
 package com.taiwanandroidapp.tim.bopomofotoenglish;
 
+import android.graphics.BitmapRegionDecoder;
 import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
@@ -13,6 +14,9 @@ import com.taiwanandroidapp.tim.bopomofotoenglish.UI.MyKeyboardView;
 
 public class TransImService extends InputMethodService {
 
+    private final int KEYBOARD_NORNAL = 1;
+    private final int KEYBOARD_UPPER = 2;
+
     private Keyboard bopomofo_keyboard;
 
     @Override
@@ -25,6 +29,18 @@ public class TransImService extends InputMethodService {
         super.onInitializeInterface();
 
         bopomofo_keyboard = new Keyboard(this,R.xml.bopomofo_keys);
+
+    }
+
+    private void changeKeyboard(int keyboard_code){
+        Keyboard keyboard;
+
+        if(keyboard_code == KEYBOARD_NORNAL){
+            keyboard = new Keyboard(this,R.xml.bopomofo_keys);
+        }else {
+            keyboard = new Keyboard(this,R.xml.bopomofo_keys_uppercase);
+        }
+
 
     }
 
@@ -45,12 +61,21 @@ public class TransImService extends InputMethodService {
 
             @Override
             public void onKey(int i, int[] ints) {
-                if(i == 8){
-                    // define in bopomofo_keys.xml
-                    // 8 is delete key signal
-                    getCurrentInputConnection().deleteSurroundingText(1,0);
-                }else {
-                    getCurrentInputConnection().commitText(String.valueOf((char) i), 1);
+
+                switch (i){
+                    case 8:
+                        // define in bopomofo_keys.xml
+                        // 8 is delete key signal
+                        getCurrentInputConnection().deleteSurroundingText(1,0);
+                        break;
+                    case -1:
+                        changeKeyboard(KEYBOARD_UPPER);
+                        break;
+                    case -2:
+                        changeKeyboard(KEYBOARD_NORNAL);
+                        break;
+                    default:
+                        getCurrentInputConnection().commitText(String.valueOf((char) i), 1);
                 }
 
             }
